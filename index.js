d3a809684c29b782ab199456df3cf8f685d7aee3
kimmy0697 <kimlesliefaina.klf@gmail.com>
2018-10-09 23:04:17 +0800

    updated and add new files

--- index.js
+++ index.js
@@ -104,6 +104,17 @@ app.get('/admin/faculty', function (req, res){
   })
 })
 
+app.get('/admin/faculty/:id', function (req, res){
+  client.query("SELECT employee_id AS employee_id, fname AS fname, lname AS lname, email AS femail, phone AS fphone, is_admin AS is_admin FROM users WHERE user_type = 'faculty';")
+  .then((faculties)=>{
+    res.render('admin/faculty_details', {
+      layout: 'admin',
+      faculties: faculties.rows
+    })
+  })
+})
+
+
 app.get('/admin/add_faculty', function (req, res){
   res.render('admin/add_faculty', {
     layout: 'admin'
@@ -111,7 +122,7 @@ app.get('/admin/add_faculty', function (req, res){
 })
 
 app.post('/add_faculty', function (req, res){
-  client.query("INSERT INTO users (fname, lname, email, password, user_type, phone, student_number, employee_id) VALUES ('" + req.body.fname + "', '" + req.body.lname + "', '" + req.body.email + "', '" + req.body.password + "', '" + req.body.user_type + "', '" + req.body.phone + "', '" + req.body.student_number + "','" + req.body.employee_id + "');")
+  client.query("INSERT INTO users (fname, lname, email, password, user_type, phone, student_number, employee_id, is_admin) VALUES ('" + req.body.fname + "', '" + req.body.lname + "', '" + req.body.email + "', '" + req.body.password + "', '" + req.body.user_type + "', '" + req.body.phone + "', '" + req.body.student_number + "','" + req.body.employee_id + "', '" + req.body.is_admin +"');")
   .then((results)=>{
     console.log('results?', results);
     res.redirect('/admin/faculty');
@@ -134,6 +145,16 @@ app.get('/admin/student', function (req, res){
   })
 })
 
+app.get('/admin/student/:id', function (req, res){
+  client.query("SELECT student_number AS snumber, fname AS fname, lname AS lname, email AS semail, phone AS sphone FROM users WHERE user_type = 'student';")
+  .then((student_details)=>{
+    res.render('admin/student_details', {
+      layout: 'admin',
+      students: students.rows
+    })
+  })
+})
+
 app.get('/admin/add_student', function (req, res){
   res.render('admin/add_student', {
     layout: 'admin'
@@ -141,7 +162,7 @@ app.get('/admin/add_student', function (req, res){
 })
 
 app.post('/add_student', function (req, res){
-  client.query("INSERT INTO users (fname, lname, email, password, user_type, is_admin, phone, student_number) VALUES ('" + req.body.fname + "', '" + req.body.lname + "', '" + req.body.email + "', '" + req.body.password + "', '" + req.body.user_type + "', '" + req.body.is_admin + "', '" + req.body.phone + "', '" + req.body.student_number + "');")
+  client.query("INSERT INTO users (fname, lname, email, password, user_type, phone, student_number) VALUES ('" + req.body.fname + "', '" + req.body.lname + "', '" + req.body.email + "', '" + req.body.password + "', '" + req.body.user_type + "', '" + req.body.phone + "', '" + req.body.student_number + "');")
   .then((results)=>{
     console.log('results?', results);
     res.redirect('/admin/student');
@@ -175,6 +196,44 @@ app.get('/admin/classes', function (req, res){
     })
   })
 })
+
+app.get('/admin/classes/:id', function (req, res){
+  client.query(`
+    SELECT classes.id AS class_id,
+      batches.batches AS batch,
+      sections.sections AS section,
+      users.id AS adviser_id,
+      users.fname AS fname,
+      users.lname AS lname
+    FROM classes
+    INNER JOIN year_levels ON year_levels.id = year_level_id
+    INNER JOIN batches ON batches.id = batch_id
+    INNER JOIN sections ON sections.id = section_id
+    INNER JOIN users ON users.id = adviser_id
+    WHERE classes.id =` + req.params.id + `;
+    `)
+  .then((class_details)=>{
+    res.render('admin/class_details', {
+      layout: 'admin',
+      batch: class_details.rows[0].batch,
+      section: class_details.rows[0].section,
+      fname: class_details.rows[0].fname,
+      lname: class_details.rows[0].lname
+    })
+  })
+})
+
+app.post('/admin/classes/:id', function (req, res){
+  client.query(``)
+  .then((results)=>{
+    console.log('results?', results);
+    res.redirect('admin/class_details');
+  })
+  .catch((err)=>{
+    console.log('error',err);
+  })
+})
+
 
 app.get('/admin/add_class', function (req, res){
   client.query("SELECT id AS id, batches AS batches FROM batches;")
